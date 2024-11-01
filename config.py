17e36442fae0f69c3edda3dc89cc3cd6a55a7008
Onlyfanswisxeza <errorcode@xezadev.com>
2024-11-02 03:41:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-nicknames = "Hax Netting NUKER"
-ban_reason = "HAXNET"
-nuke_message = """
-> :bomb:  NUKED BY YOU
-> :link: YOU
+nicknames = "Xena"
+ban_reason = "Xeza"
+nuke_message = "imagine underestimating"
+> :bomb:  NUKED BY Xena
+> :link: discord.gg/drunken
 > :heavy_plus_sign:  @everyone @here
 """
-role_rename = "Hax Netting"
+role_rename = "xena"
